4670c46ec8832ab05781d13be7c45ec6a1845e2c
jimmy618 <wong124816@gmail.com>
2022-05-17 10:01:39 +0800

    search request interface rework

--- app/src/main/java/edu/cuhk/csci3310/project/searchRequest/MainSearchRequestActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/project/searchRequest/MainSearchRequestActivity.java
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +18,7 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 
 import java.util.Locale;
+import java.util.Calendar;
 
 import edu.cuhk.csci3310.project.MainActivity;
 import edu.cuhk.csci3310.project.R;
@@ -29,14 +30,18 @@ public class MainSearchRequestActivity extends AppCompatActivity {
 
     // reuse favor model for searching
     String favorType;
-    final Calendar myCalendar= Calendar.getInstance();
-    String selectedDate;
+    EditText enquirerName;
+    EditText startDateView;
+    EditText endDateView;
+    String selectedStartDate;
+    String selectedEndDate;
+
     // use visibility to control option menu on screen
     // fragment is another option, but it is not necessary in the simple case
     View currentView; // variable to store current view
     View allView;
-    View BorrowingView; String itemType; EditText dateView;
-    View DiningView; int participantNum; EditText query_Dining_date;
+    View BorrowingView; String itemType;
+    View DiningView; int participantNum;;
     View GatheringView;
     View MovingView;
     View TutoringView;
@@ -55,6 +60,64 @@ public class MainSearchRequestActivity extends AppCompatActivity {
         DiningView = findViewById(R.id.query_Dining);
         currentView = allView; // user looking at allView
 
+        // enquirerName input edittext preparation
+        enquirerName = findViewById(R.id.query_name_input);
+
+        // start date and end date input preparation
+        startDateView = findViewById(R.id.query_start_date);
+        endDateView = findViewById(R.id.query_end_date);
+
+        startDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                DatePickerDialog picker = new DatePickerDialog(MainSearchRequestActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                startDateView.setText(+ year + "/" + (monthOfYear + 1) + "/" + dayOfMonth);
+                            }
+                        }, year, month, day);
+                picker.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == DialogInterface.BUTTON_NEGATIVE) {
+                            startDateView.setText("");
+                        }
+                    }
+                });
+                picker.show();
+            }
+        });
+        endDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                DatePickerDialog picker = new DatePickerDialog(MainSearchRequestActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                endDateView.setText(+ year + "/" + (monthOfYear + 1) + "/" + dayOfMonth);
+                            }
+                        }, year, month, day);
+                picker.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == DialogInterface.BUTTON_NEGATIVE) {
+                            endDateView.setText("");
+                        }
+                    }
+                });
+                picker.show();
+            }
+        });
+
         Spinner spinner = (Spinner) findViewById(R.id.query_Borrowing_spinner);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -62,43 +125,14 @@ public class MainSearchRequestActivity extends AppCompatActivity {
                 itemType = (String) adapterView.getItemAtPosition(pos);
                 // Log.d(TAG, "get item : " + item);
             }
-
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
+            public void onNothingSelected(AdapterView<?> adapterView) {}
         });
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.request_borrowing_type, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        // dateView = findViewById(R.id.query_date);
-        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH,month);
-                myCalendar.set(Calendar.DAY_OF_MONTH,day);
-                updateLabel();
-            }
-        };
-        View.OnClickListener l = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new DatePickerDialog(MainSearchRequestActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        };
-        dateView = findViewById(R.id.query_date);
-        query_Dining_date = findViewById(R.id.query_Dining_date);
-        View array[] = new View[]{dateView, query_Dining_date };
-        for (View view : array){
-            view.setOnClickListener(l);
-        }
-
-        String myFormat="dd/MM/yyyy";
-        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
-        selectedDate = dateFormat.format(myCalendar.getTime());
     }
 
     public void onTypeClicked(View view) {
@@ -112,14 +146,14 @@ public class MainSearchRequestActivity extends AppCompatActivity {
                     break;
             case R.id.radio_Borrowing:
                 if (checked) {
-                    dateView.setText(selectedDate);
+                    // dateView.setText(selectedDate);
                     changeView(currentView, BorrowingView); favorType = "Borrowing";
 
                 }
                     break;
             case R.id.radio_Dining:
                 if (checked) {
-                    query_Dining_date.setText(selectedDate);
+                    // query_Dining_date.setText(selectedDate);
                     changeView(currentView, DiningView); favorType = "Dining";
 
                 }
@@ -152,12 +186,18 @@ public class MainSearchRequestActivity extends AppCompatActivity {
         Intent intent = new Intent(MainSearchRequestActivity.this, searchResultActivity.class);
         Bundle extras = new Bundle();
         extras.putString("favorType", favorType);
+        extras.putString("enquirerName", enquirerName.getText().toString());
+        // Log.d(TAG, "get the name: " + enquirerName.getText() + " is null: " + enquirerName.getText().toString().isEmpty());
+        extras.putString("startDate", startDateView.getText().toString());
+        extras.putString("endDateView", endDateView.getText().toString());
+        //Log.d(TAG, "get date: " + startDateView.getText() + " " + endDateView.getText());
+        //Log.d(TAG, "is empty: " + startDateView.getText().toString().isEmpty() + " " + endDateView.getText().toString().isEmpty());
         switch(favorType){
             case "all":
                 break;
             case "Borrowing":
                 extras.putString("itemType", itemType);
-                extras.putString("date", selectedDate);
+                // extras.putString("date", selectedDate);
                 //Log.d(TAG, "get date: " + dateView.getText());
                 break;
             case "Dining":
@@ -180,12 +220,4 @@ public class MainSearchRequestActivity extends AppCompatActivity {
         currentView = e2;
     }
 
-    private void updateLabel(){
-        String myFormat="dd/MM/yyyy";
-        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
-        String date = dateFormat.format(myCalendar.getTime());
-        selectedDate = date;
-        dateView.setText(selectedDate);
-        query_Dining_date.setText(selectedDate);
-    }
 }
